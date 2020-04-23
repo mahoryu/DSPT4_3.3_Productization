e93790cbdf20bc263c62a024211f7d6f018b30e1
mahoryu <mahoryu@gmail.com>
2020-04-22 19:32:19 -0600

    added basic html pages

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -13,19 +13,19 @@ def list_books():
     ]
     return jsonify(books)
 
-# @book_routes.route("/books")
-# def list_books_for_humans():
-#     books = [
-#         {"id": 1, "title": "Book 1"},
-#         {"id": 2, "title": "Book 2"},
-#         {"id": 3, "title": "Book 3"},
-#     ]
-#     return render_template("books.html", message="Here's some books", books=books)
+@book_routes.route("/books")
+def list_books_for_humans():
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"},
+    ]
+    return render_template("books.html", message="Here's some books", books=books)
 
-# @book_routes.route("/books/new")
-# def new_book():
-#     return render_template("new_book.html")
-#
+@book_routes.route("/books/new")
+def new_book():
+    return render_template("new_book.html")
+
 # @book_routes.route("/books/create", methods=["POST"])
 # def create_book():
 #     print("FORM DATA:", dict(request.form))
