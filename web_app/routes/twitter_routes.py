de81e301f44d15ea030fd99f2ce81bf67242306b
mahoryu <mahoryu@gmail.com>
2020-04-25 11:02:04 -0600

    store user in the database

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -15,7 +15,12 @@ def fetch_user_data(screen_name):
     user = twitter_api.get_user(screen_name)
 
     # store user info in database
-    db_user = User(__________)
+    db_user = User.query.get(user.id) or User(id=user.id)
+    db_user.screen_name = user.screen_name
+    db_user.name = user.name
+    db_user.location = user.location
+    db_user.followers_count = user.followers_count
+
     db.session.add(db_user)
     db.session.commit()
 
