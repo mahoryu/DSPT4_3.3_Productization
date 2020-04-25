c0bd34341a3fb6409619fa093ee8c8d3009fc3c5
mahoryu <mahoryu@gmail.com>
2020-04-25 11:14:54 -0600

    Store embeddings

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -4,6 +4,7 @@ from flask import Blueprint, jsonify, request, render_template, flash, redirect
 
 from web_app.models import db, User, Tweet, parse_records
 from web_app.services.twitter_service import api as twitter_api
+from web_app.services.basilica_service import connection as basilica_connection
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
@@ -46,10 +47,13 @@ def fetch_user_data(screen_name):
         db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
         db_tweet.user_id = status.author.id
         db_tweet.full_text = status.full_text
-        # embedding = basilica_client.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
+        #
+        # fetching corresponding embedding
+        #
+        embedding = basilica_connection.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
         # embedding = embeddings[counter]
-        # print(len(embedding))
-        # db_tweet.embedding = embedding
+        print(len(embedding))
+        db_tweet.embedding = embedding
         db.session.add(db_tweet)
         #counter += 1
 
--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -9,11 +9,13 @@ BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
 connection = basilica.Connection(BASILICA_API_KEY)
 print(type(connection))
 
-embedding = connection.embed_sentence('hey this is a cool tweet', model='twitter')
-print(embedding)
+if __name__ == "__main__":
 
-tweets = ["hello world", "artificail intelligence", "anther tweet here #cool"]
-embeddings = connection.embed_sentences(tweets, model="twitter")
-for embed in embeddings:
-    print("----")
-    print(len(embed))
+    embedding = connection.embed_sentence('hey this is a cool tweet', model='twitter')
+    print(embedding)
+
+    tweets = ["hello world", "artificail intelligence", "anther tweet here #cool"]
+    embeddings = connection.embed_sentences(tweets, model="twitter")
+    for embed in embeddings:
+        print("----")
+        print(len(embed))
